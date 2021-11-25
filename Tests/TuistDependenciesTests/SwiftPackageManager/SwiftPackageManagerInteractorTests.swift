a7221bb5ddfc9ea913a95d919b26a9f42657388f
Daniele Formichelli <df@bendingspoons.com>
2021-11-25 08:46:32 +0100

    chore: swiftformat

--- Tests/TuistDependenciesTests/SwiftPackageManager/SwiftPackageManagerInteractorTests.swift
+++ Tests/TuistDependenciesTests/SwiftPackageManager/SwiftPackageManagerInteractorTests.swift
@@ -102,7 +102,7 @@ final class SwiftPackageManagerInteractorTests: TuistUnitTestCase {
             swiftPackageManagerDirectory,
             [
                 ".build",
-                "Package.swift"
+                "Package.swift",
             ]
         )
         try XCTAssertDirectoryContentEqual(
@@ -194,7 +194,7 @@ final class SwiftPackageManagerInteractorTests: TuistUnitTestCase {
             swiftPackageManagerDirectory,
             [
                 ".build",
-                "Package.swift"
+                "Package.swift",
             ]
         )
         try XCTAssertDirectoryContentEqual(
@@ -282,7 +282,7 @@ final class SwiftPackageManagerInteractorTests: TuistUnitTestCase {
             swiftPackageManagerDirectory,
             [
                 ".build",
-                "Package.swift"
+                "Package.swift",
             ]
         )
         try XCTAssertDirectoryContentEqual(
